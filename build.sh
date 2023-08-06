89f3908ef364c79f088d472fc241ee58b5da1985
buihien224 <buihien224@gmail.com>
2023-08-06 19:23:27 +0700

    update more vi

--- build.sh
+++ build.sh
@@ -48,3 +48,6 @@ rm -rf $(find -type d -name build)
 #sed -i "s/targetSdkVersion: '28'/targetSdkVersion: '33'/g" $(find -type f -name apktool.yml)
 #sed -i "s/minSdkVersion: '28'/minSdkVersion: '31'/g" $(find -type f -name apktool.yml)
 #sed -i "s/compileSdkVersionCodename="6.0-2438415"/compileSdkVersionCodename="6.0-2438415"/g" $(find -type f -name apktool.yml)
+
+
+read -p "Nhấn Enter để dừng script: " input
